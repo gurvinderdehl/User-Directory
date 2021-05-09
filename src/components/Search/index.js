78574b6d8c46d1d7bed1ed16c28f265938460134
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-09 21:12:09 +0100

    Search functionality added

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,11 +2,21 @@ import React from "react"
 
 function Search() {
     return (
-       <div>
-           <div>
-               
-           </div>
-       </div>
+        <div className="container">
+            <div className="col-8">
+                <input type="search"
+                    className="form-control"
+                    aria-describedby="search-help"
+                    placeholder="Search"></input>
+            </div>
+
+
+            <div className="col-4">
+                <button
+                    type="submit"
+                    className="btn main-btn btn-primary">Search</button>
+            </div>
+        </div>
     )
 
 }
